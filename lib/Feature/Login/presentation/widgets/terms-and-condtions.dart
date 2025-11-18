5496a1f51b1af41df59f305a08e7c422ed182328
habibaaa682 <habibagalall2002@gmail.com>
2025-11-18 19:34:53 +0200

    terms and condtions widgets

---
+++ lib/Feature/Login/presentation/widgets/terms-and-condtions.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:nuzul/Core/utiles/app-text-styles.dart';
+import 'package:nuzul/Core/utiles/app_colors.dart';
+import 'package:nuzul/Feature/Login/presentation/widgets/custom-checkbox.dart';
+
+class TermsAndCondtions extends StatefulWidget {
+  const TermsAndCondtions({super.key});
+
+  @override
+  State<TermsAndCondtions> createState() => _TermsAndCondtionsState();
+}
+
+class _TermsAndCondtionsState extends State<TermsAndCondtions> {
+  bool isTermsAccepted = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CustomCheckBox(
+          onChanged: (value) {
+            isTermsAccepted = value;
+            setState(() {});
+          },
+          isChecked: isTermsAccepted,
+        ),
+        SizedBox(width: 16),
+        Text(
+          'By signing up, you agree to our ',
+          style: TextStyles.semiBold13.copyWith(color: Colors.grey),
+        ),
+        Text(
+          'Terms & Conditions',
+          style: TextStyles.semiBold13.copyWith(color: AppColors.textDark),
+        ),
+      ],
+    );
+  }
+}
