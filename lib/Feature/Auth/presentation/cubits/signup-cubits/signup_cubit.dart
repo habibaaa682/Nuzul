9afe5f9b802c602478408be0abcf448500fdf9fa
habibaaa682 <habibagalall2002@gmail.com>
2025-11-20 20:50:54 +0200

    Apply Cubit

---
+++ lib/Feature/Auth/presentation/cubits/signup-cubits/signup_cubit.dart
@@ -0,0 +1,36 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:nuzul/Feature/Auth/domain/entities/user-entity.dart';
+import 'package:nuzul/Feature/Auth/domain/repos/auth-repo.dart';
+
+part 'signup_state.dart';
+
+class SignupCubit extends Cubit<SignupState> {
+  final AuthRepo authRepo;
+  SignupCubit(this.authRepo) : super(SignupInitial());
+
+  Future<void> signUp(
+    String firstName,
+    String lastName,
+    String userName,
+    String phoneNumber,
+    String email,
+    String password,
+    int userType,
+  ) async {
+    emit(SignupLoading());
+    final result = await authRepo.signUp(
+      firstName,
+      lastName,
+      userName,
+      phoneNumber,
+      email,
+      password,
+      userType,
+    );
+    result.fold(
+      (failure) => emit(SignupFailure(failure.toString())),
+      (user) => emit(SignupSuccess(user: user)),
+    );
+  }
+}
